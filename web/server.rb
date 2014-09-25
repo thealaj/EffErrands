d2aca99ff9743c26e648822a2548daca5c6b3144
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-25 15:47:18 -0500

    Merged with Lili's files.

--- web/server.rb
+++ web/server.rb
@@ -1,12 +1,16 @@
 require_relative '../lib/efferrands.rb'
 require 'sinatra'
 require 'pry-byebug'
+require 'unirest'
+require 'dotenv'
 
 class EffErrands::Server < Sinatra::Application
 
   set :bind, "0.0.0.0"
+  Dotenv.load
 
   get '/' do
+    binding.pry
     #home page
     @@user_items = []
     @@start_location = []
@@ -86,7 +90,32 @@ class EffErrands::Server < Sinatra::Application
   end
   #@@addresses = {:endpoint=>true, :origins=>["1803+E+18th+Street+Austin+TX", "2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751"], :destinations=>["2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751", "800+Brazos+St+Austin+TX"]}
 
+  get '/api_request' do
+    en_url = URI.encode('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + @@address[:origins].join("|") + '&destinations='+ @@address[:destinations].join("|") + '&units=imperial&key=' + ENV['GOOGLE_MAPS_KEY'])
+    response = Unirest.get (en_url)
+    data = response.body
+    algo_data = []
+    i = 0
+    while i < data['origin_addresses'].length
+      org_ary = []
+      j = 0
+      while j < data['destination_addresses'].length
+        dest_ary = []
+        dest_ary << data['destination_addresses'][j]
+        dest_ary << data['rows'][i]['elements'][j]['distance']['value']
+        org_ary << dest_ary
+        j += 1
+      end
+      org_hash = {}
+      org_hash[data['origin_addresses'][i]] = org_ary
+      algo_data << org_hash
+      i += 1
+    end
+
+  end
+
 
   run! if __FILE__ == $0
 end
+
 

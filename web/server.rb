f1f2a57e63af1f2cac09907e4b086da42b3b06b2
Lili Serfaty <lili.serfaty@gmail.com>
2014-09-26 08:52:48 -0500

    Add a new get for google API requests using their waypoints feature. This is a fallback option to use their travelling salesman sorting feature. this get request receives sorted data from google in optimized travel order, then returns the data in a hash. the key is the point in the order of the optimized trip. the value is the address and the distance TO the address.

--- web/server.rb
+++ web/server.rb
@@ -95,25 +95,36 @@ class EffErrands::Server < Sinatra::Application
   end
   #@@addresses = {:endpoint=>true, :origins=>["1803+E+18th+Street+Austin+TX", "2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751"], :destinations=>["2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751", "800+Brazos+St+Austin+TX"]}
  
-  # USING BEN'S ALGORITHM:
+  # USING BEN'S AWESOME ALGORITHM:
   get '/api_request' do
+    # Make API call to Google:
     en_url = URI.encode('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + @@address[:origins].join("|") + '&destinations='+ @@address[:destinations].join("|") + '&units=imperial&key=' + ENV['GOOGLE_MAPS_KEY'])
+    # Recieve code vomit:
     response = Unirest.get (en_url)
+    # Orders code vomit in reasonable JSON:
     data = response.body
+    # Empty hash for Ben's data:
     algo_data = []
     i = 0
+    # Loop through origin addresses:
     while i < data['origin_addresses'].length
       org_ary = []
       j = 0
+      # Loop through destination addresses:
       while j < data['destination_addresses'].length
         dest_ary = []
+        # Push each destination into the array:
         dest_ary << data['destination_addresses'][j]
+        # Push each destination's distance into the array:
         dest_ary << data['rows'][i]['elements'][j]['distance']['value']
+        # Push the distination array into the origins array:
         org_ary << dest_ary
         j += 1
       end
       org_hash = {}
+      # Create a hash of origin address keys and their data values:
       org_hash[data['origin_addresses'][i]] = org_ary
+      # Push hashes into Ben's array:
       algo_data << org_hash
       i += 1
     end
@@ -121,10 +132,30 @@ class EffErrands::Server < Sinatra::Application
   end
 
   #USING GOOGLE'S SHITTY ALGORITHM:
+  data['routes'].first['legs'].first['steps'].first['html_instructions']
   get '/api_request_waypoints' do
-    url = URI.encode('https://maps.googleapis.com/maps/api/directions/json?origin=' + @@address[:origins].first + '&destination=' + @@address[:destinations].last + '&waypoints=opitimization:true' + @@address[:origins][1..-2].join("|") + '&key=' + ENV['GOOGLE_MAPS_KEY'])
-    ordered_response = Unirest.get (url)
+    # Make API call to google maps: 
+    new_url = URI.encode('https://maps.googleapis.com/maps/api/directions/json?origin=' + @@address[:origins].first + '&destination=' + @@address[:origins].first + '&waypoints=optimize:true|' + @@address[:origins][1..-1].join("|") + '&key=' + ENV['GOOGLE_MAPS_KEY'])
+    ordered_response = Unirest.get (new_url)
+    # Put the API response in some sort of order:
     data = ordered_response.body
+
+    # Creates some containers for the data:
+    points_hash = {}
+    each_stop = []
+    # Adds all of the addresses into an array in the order sorted by Google:
+    data['routes'].first['legs'].each_index do |i|
+      each_stop << "#{data['routes'].first['legs'][i]['start_address']}: #{data['routes'].first['legs'][i]['distance']['text']}"
+    end
+    ## Adds the start point back to the end of the array:
+    # each_stop.push(data['routes'].first['legs'].first['start_address'])
+    # Adds a key for each address value denoting the order of the trip:
+    each_stop.each_index do |i|
+      points_hash[i+1] = each_stop[i]
+    end
+
+
+
   end
 
 
